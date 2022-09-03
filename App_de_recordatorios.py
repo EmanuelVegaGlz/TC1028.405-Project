59b0704e139102e35d67b8410ebd0116063c9c0e
EmanuelVegaGlz <111370943+EmanuelVegaGlz@users.noreply.github.com>
2022-09-02 21:29:15 -0500

    Add files via upload

--- App_de_recordatorios.py
+++ App_de_recordatorios.py
@@ -1,59 +1,48 @@
 # App de recordatorios
 
 from datetime import datetime #Importe un modulo para hacer uso de fechas
-contador_a = int(1)           #Contador para llevar un recuento de las tareas
-veces = int(0)                #Sirve como una variable para contar la cantidad de veces que se ve y almacena una tarea
+contador_a = int(0)           #Contador para llevar un recuento de las tareas
 
-def total_de_tareas (contador_a, veces):        
-      tottareas = contador_a * veces
-      return tottareas
+
 
 #Esta es la funcion para visualizar las tareas, una vez que ingresamos las tareas
 
-def vista_tarea (total_de_tareas):           
-    mostrar_tareas = ("Tarea 1", agregar_tarea, "Tarea 2", agregar_tarea, "Tarea 3", agregar_tarea)
-    
-    return ver_tareas
+def vista_tarea (tarea):           
+    mostrar_tareas = ["Tarea 1", tarea, "Tarea 2", tarea, "Tarea 3", tarea]  #Aqui estoy tratando de crear una matriz :)
+    return mostrar_tareas
 
-def conocer_tareas_hoy (lista):
-      tareas_de_hoy = lista(fecha == datetoday)
-      return tareas_de_hoy
+#Esta es la funcion para visualizar las tareas programadas para hoy
+def conocer_tareas_hoy (tarea):
+      pendientes = tarea ([date] == datetime)  #No estoy seguro si es la forma correcta para comparar la fecha de la lista/ 
+      return pendientes                        #con la fecha del dia para poder determinar las tareas para hoy, pero esa es la idea.
 
-def conocer_tareas_pendientes (lista):
-      pendientes = lista(fecha < datetoday)
+#Funcion para conocer las tareas pendientes
+def conocer_tareas_pendientes (tarea):
+      pendientes = tarea ([date] < datetime)
       return pendientes
-
-#Esta es una funcion que permite agregar una tarea especificando la materia, la fecha y una breve descripcion de la misma
-def agregar_tarea (materia, descripcion,date,):
-     materia = str (input("Especifique la materia de la tarea"))
-     descripcion = str (input("Escriba una breve descripcion de su tarea"))
-     date = datetime(input ("Cuando lo entrega"))
-     nueva_tarea = [materia, descripcion,date]
-     return nueva_tarea 
-
       
-"""Despues de esta funcion me gustaria crear un arreglo en donde tenga una matriz con las respuetas del usuario
-y poder ir almacenadno las tareas con sus fechas y demas parametros """
-
 #Aqui inicia la ejecucion del codigo   
 print ("Hola que gusto volver a vernos ¿Que deseas hacer?")
-print ("Para ver tus tareas ingresa 1")
-print ("Para ver las tareas que tienes programadas para hoy digita 2")
-print ("Si deseas añadir una tarea ingrese 3")
-print ("Por el contrario si lo que quiere es conocer las tareas programadas para hoy escriba 4")
-print ("Desea visualizar sus tareas pasadas seleccione 5")
+print ("Si desea añadir una tarea ingrese 1")
+print ("Para ver sus tareas ingresa 2")
+print ("Desea visualizar sus tareas que tienes programadas para hoy digite 3")
+print ("Por el contrario si lo que quiere es conocer sus tareas pasadas seleccione 4")
+
 seleccion_1 = int (input ("Escriba la opcion deseada: "))
 
 if seleccion_1 == 1:
-      vista_tarea (parametros)  
-      print (vista_tarea)
+      materia = str (input("Especifique la materia de la tarea: "))
+      descripcion = str (input("Escriba una breve descripcion de su tarea: "))
+      date = datetime(input ("¿Cuando lo entrega?: "))
+      tarea = [materia, descripcion,date]
+      contador_a = contador_a + 1
 
-if seleccion_1 == 2: 
-      conocer_tareas_hoy (lista)
-      print (conocer_tareas_hoy)
+if seleccion_1 == 2:
+      ver = vista_tarea(tarea)  
+      print (ver)
 
-if seleccion_1 != 1 or 2:
-      print ("Opcion invalida, por favor ingrese 1 o 2")
-      seleccion_1 (input())
-else:
-      print("!Gracias por organizarte con nosotros vuelve pronto¡")
+if seleccion_1 == 3:
+      print ("Sus tareas programadas para hoy son: ", conocer_tareas_pendientes(tarea))
+
+if seleccion_1 == 4:
+      print ("Sus tareas pendientes son: ", conocer_tareas_pendientes(tarea))
