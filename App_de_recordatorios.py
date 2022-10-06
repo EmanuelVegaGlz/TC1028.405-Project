f45e1e93bf8420f61a81795bb6b391ddca592c1d
EmanuelVegaGlz <111370943+EmanuelVegaGlz@users.noreply.github.com>
2022-10-06 08:07:36 -0500

    Add files via upload

--- App_de_recordatorios.py
+++ App_de_recordatorios.py
@@ -1,46 +1,88 @@
-# App de recordatorios
+"""
+Proyecto final TC1028.
+Pensamiento Computacional para ingenieria.
+Profesor: Benjamín Valdés Aguirre.
+Nombre: Emanuel Josué Vega González.
+ID: A01710366.
+El programa funciona como una aplicacion de tareas, que se guardan en listas,
+el usuario puede ver sus tareas de acuerdo al dia que se entrega, el programa 
+busca la fecha dentro de la matriz.
+"""
 
 # Bibliotecas
 
 from datetime import date
 from datetime import datetime
-
-#Funcion para agregar tareas
-
-def agregar_tarea ():
-      opcion_añadir = int(input("Deseas agregar una tarea? Si=1, No=0"))
-      while opcion_añadir == 1:
-            tarea = str(input())
-
-#Esta es la funcion para visualizar las tareas programadas para hoy
-def conocer_tareas_hoy (matriz):  
-      today = date.today()
-      for i in matriz:
-            if i == today:
-                  print(i)  
+from itertools import chain
 
 """
-Con las funciones para comparar las tareas tengo un problema con la biblioteca,
-pero de ahi en fuera ya arregle los problemas del avance pasado
-"""                          
+================== funciones principales =======================================
+"""
 
-#Funcion para conocer las tareas pendientes
-def conocer_tareas_pendientes (matriz):
-      today = date.today()
+#Esta es la funcion para visualizar las tareas programadas para hoy
+def conocer_tareas_hoy(matriz):  
+      today = 4
       for i in matriz:
-            if i < today:
+            if i[2] == today:
+                  print(i)  
+
+                     
+#Funcion para conocer las tareas pasadas
+def conocer_tareas_pasadas(matriz):
+      today = 4
+      for i in matriz:
+            if i[2] < today:
                   print(i)
-      
 
-      
+#Mensaje de bienvendida
+def bienvenida():    
+      a = ("Hola que gusto volver a vernos ¿Que deseas hacer?")
+      b = ("Si desea añadir una tarea ingrese 1")
+      c = ("Para ver sus tareas ingresa 2")
+      d = ("Desea visualizar sus tareas que tienes programadas para hoy digite 3")
+      e = ("Si lo que quiere es conocer sus tareas pasadas seleccione 4")
+      mensaje = [
+            [a],
+            [b],
+            [c],
+            [d],
+            [e],
+      ]
+      return mensaje
+
+def imprime_mensaje():
+      imprimir = bienvenida()
+      for i in imprimir:
+            print(i)
+
+def imprime_por_opcion(seleccion):
+      descartar = bienvenida()
+      descartar.remove(0)
+      if seleccion == 1:
+            descartar.remove(1)
+            for i in descartar:
+                  print(i)
+      if seleccion == 2:
+            descartar.remove(2)
+            for i in descartar:
+                  print(i)
+      if seleccion == 3:
+            descartar.remove(3)
+            for i in descartar:
+                  print(i)
+      if seleccion == 4:
+            descartar.remove(4)
+            for i in descartar:
+                  print(i)
+
+
+
 #Aqui inicia la ejecucion del codigo   
-print ("Hola que gusto volver a vernos ¿Que deseas hacer?")
-print ("Si desea añadir una tarea ingrese 1")
-print ("Para ver sus tareas ingresa 2")
-print ("Desea visualizar sus tareas que tienes programadas para hoy digite 3")
-print ("Por el contrario si lo que quiere es conocer sus tareas pasadas seleccione 4")
-
+imprime_mensaje()
 seleccion_1 = int (input ("Escriba la opcion deseada: "))
+while seleccion_1 < 1 or seleccion_1 > 4:
+      print("Opcion invalida, vuelve a intentarlo")
+      seleccion_1 = int (input ("Escriba la opcion deseada: "))
 
 if seleccion_1 == 1:
       seleccion_2 = int(input("Deseas agregar una tarea? Si=1, No=0: "))
@@ -68,12 +110,12 @@ if seleccion_1 == 2:
       seleccion_1 = int (input ("Escriba la opcion deseada: "))
 
 if seleccion_1 == 3:
-      print ("Sus tareas programadas para hoy son: ", conocer_tareas_pendientes(tarea))
+      print ("Sus tareas programadas para hoy son: ", conocer_tareas_hoy(mat))
       print ("Quiere conocer sus tareas pasadas seleccione 4")
       seleccion_1 = int (input ("Escriba la opcion deseada: "))
 
 if seleccion_1 == 4:
-      print ("Sus tareas pendientes son: ", conocer_tareas_pendientes(tarea))
+      print ("Sus tareas pendientes son: ", conocer_tareas_pasadas(mat))
 else:
       print("Opcion invalida")
 
