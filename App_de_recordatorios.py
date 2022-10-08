fe1688e9c55719eae642db602713c6d5abd951f7
EmanuelVegaGlz <111370943+EmanuelVegaGlz@users.noreply.github.com>
2022-10-07 23:02:58 -0500

    Add files via upload

--- App_de_recordatorios.py
+++ App_de_recordatorios.py
@@ -13,7 +13,6 @@ busca la fecha dentro de la matriz.
 
 from datetime import date
 from datetime import datetime
-from itertools import chain
 
 """
 ================== funciones principales =======================================
@@ -21,18 +20,33 @@ from itertools import chain
 
 #Esta es la funcion para visualizar las tareas programadas para hoy
 def conocer_tareas_hoy(matriz):  
-      today = 4
+      today = date.today()
+      dia_actual = today.day
+      mes_actual = today.month
+      año_actual = today.year
+
       for i in matriz:
-            if i[2] == today:
-                  print(i)  
+            for j in i[2]:
+                  for k in j:
+                        if j[0] == año_actual and j[1] == mes_actual and j[2] == dia_actual:
+                              print(i)  
 
                      
 #Funcion para conocer las tareas pasadas
 def conocer_tareas_pasadas(matriz):
-      today = 4
+      today = date.today()
       for i in matriz:
-            if i[2] < today:
-                  print(i)
+            for j in i:
+                  if j[2] < today:
+                        print(i)
+
+#Funcion para conocer las tareas para mañana
+def conocer_tareas_mañana(matriz):
+      today = date.today()
+      for i in matriz:
+            for j in i:
+                  if j[2] == today + 1:
+                        print(i)
 
 #Mensaje de bienvendida
 def bienvenida():    
@@ -42,80 +56,82 @@ def bienvenida():
       d = ("Desea visualizar sus tareas que tienes programadas para hoy digite 3")
       e = ("Si lo que quiere es conocer sus tareas pasadas seleccione 4")
       mensaje = [
-            [a],
-            [b],
-            [c],
-            [d],
-            [e],
+            a,
+            b,
+            c,
+            d,
+            e
       ]
       return mensaje
-
-def imprime_mensaje():
-      imprimir = bienvenida()
-      for i in imprimir:
+#Funcion para imprimir fila por fila
+def imprime_mensaje(lista):
+      for i in lista:
             print(i)
 
-def imprime_por_opcion(seleccion):
-      descartar = bienvenida()
-      descartar.remove(0)
-      if seleccion == 1:
-            descartar.remove(1)
-            for i in descartar:
-                  print(i)
-      if seleccion == 2:
-            descartar.remove(2)
-            for i in descartar:
-                  print(i)
-      if seleccion == 3:
-            descartar.remove(3)
-            for i in descartar:
-                  print(i)
-      if seleccion == 4:
-            descartar.remove(4)
-            for i in descartar:
-                  print(i)
+#Funcion para validar fecha
+def valida(min,max,date):
+      while date < min or date > max:
+            print("Dia ", date, " inexistente, intente de nuevo")
+            date = int(input("Introduzca un valor real: "))
 
 
+#Aqui inicia la ejecucion del codigo
 
-#Aqui inicia la ejecucion del codigo   
-imprime_mensaje()
-seleccion_1 = int (input ("Escriba la opcion deseada: "))
+imprime_mensaje(bienvenida())
+seleccion_1 = int(input("Escriba la opcion deseada (Solo numeros): "))
 while seleccion_1 < 1 or seleccion_1 > 4:
       print("Opcion invalida, vuelve a intentarlo")
-      seleccion_1 = int (input ("Escriba la opcion deseada: "))
+      seleccion_1 = int(input("Escriba la opcion deseada: "))
 
 if seleccion_1 == 1:
-      seleccion_2 = int(input("Deseas agregar una tarea? Si=1, No=0: "))
+      seleccion_2 = int(input("Deseas agregar una tarea? Si = 1, No != 1: "))
       mat = []
       while seleccion_2 == 1:
             materia = str(input("Especifique la materia de la tarea: "))
             descripcion = str(input("Escriba una breve descripcion de su tarea: "))
-            date_dia = int(input ("¿Que dia lo entrega?: "))
-            date_mes = int(input ("¿En que mes lo entrega?: "))
-            date_año = int(input ("¿De que año?: "))
-            tarea = [materia, descripcion, date_dia, date_mes, date_año]
-            mat.append (tarea)
+            dia = int(input("¿Que dia lo entrega? (Solo numeros): "))
+            while dia < 1 or dia > 31:
+                  print("Dia ", dia, " inexistente, intente de nuevo")
+                  dia = int(input("¿Que dia lo entrega?"))
+            mes = int(input("¿En que mes lo entrega? (Solo numeros): "))
+            while mes < 1 or mes > 12:
+                  print("Mes ", mes, " inexistente, intente de nuevo")
+                  mes = int(input("¿En que mes lo entrega?: "))
+            año = int(input("¿De que año? (Solo numeros): "))
+            while año < 1:
+                  print("Año ", año, " inexistente, intente de nuevo")
+                  año = int(input("¿De que año?: "))
+            fecha = (año,mes,dia)
+            tarea = [materia,descripcion,fecha]
+            mat.append(tarea)
             print("Tarea creada con exito") 
             seleccion_2 = int(input("Deseas agregar una tarea? Si=1, No=0: "))
-            
-      print ("Para ver sus tareas ingresa 2")
-      print ("Desea visualizar sus tareas que tienes programadas para hoy digite 3")
-      print ("Quiere conocer sus tareas pasadas seleccione 4")
-      seleccion_1 = int (input ("Escriba la opcion deseada: "))
+      
+      rango_bienvenida = bienvenida()
+      for i in range(2,5):
+            print(rango_bienvenida [i])
+      seleccion_1 = int(input("Escriba la opcion deseada: "))
 
 if seleccion_1 == 2:
-      print (mat)
-      print ("Desea visualizar sus tareas que tienes programadas para hoy digite 3")
-      print ("Quiere conocer sus tareas pasadas seleccione 4")
-      seleccion_1 = int (input ("Escriba la opcion deseada: "))
+      imprime_mensaje(mat)
+      rango_bienvenida = bienvenida()
+      for i in range(3,5):
+            print(rango_bienvenida [i])
+      seleccion_1 = int(input("Escriba la opcion deseada: "))
 
 if seleccion_1 == 3:
-      print ("Sus tareas programadas para hoy son: ", conocer_tareas_hoy(mat))
-      print ("Quiere conocer sus tareas pasadas seleccione 4")
-      seleccion_1 = int (input ("Escriba la opcion deseada: "))
+      print("Sus tareas programadas para hoy son: ")
+      t_hoy = conocer_tareas_hoy(mat)
+      
+
+      rango_bienvenida = bienvenida()
+      print(rango_bienvenida(4))
+      seleccion_1 = int(input("Escriba la opcion deseada: "))
 
 if seleccion_1 == 4:
-      print ("Sus tareas pendientes son: ", conocer_tareas_pasadas(mat))
+      print("Sus tareas pendientes son: ")
+      conocer_tareas_pasadas(mat)
+
 else:
       print("Opcion invalida")
 
